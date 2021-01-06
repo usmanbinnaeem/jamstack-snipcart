ed49e1253a51688af2981b4ff51c99b166fdc847
usmanbinnaeemm <usmaanbinnaeem@gmail.com>
2021-01-06 18:38:05 +0500

    ADDED PRODUCTS LIST

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,6 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
 require("dotenv").config({ path: ".env" })
 module.exports = {
-  /* Your site config here */
+  
   plugins: [
     {
       resolve: "gatsby-plugin-snipcartv3",
