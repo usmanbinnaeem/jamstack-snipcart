8634b589ea4f0c915f1410c8b62d88b36705d568
usmanbinnaeemm <usmaanbinnaeem@gmail.com>
2021-01-06 19:40:00 +0500

    fix error in functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     class = "snipcart-add-item"
     data-item-id = "t-shirt"
     data-item-price = "20.00"
-    data-item-url = "/"
+    data-item-url = "https://jamstack-snipcart.netlify.app/Products"
     data-item-name = 't-shirt'
     data-item-description = 'this is t-shirt'
     data-item-image = 'https://cdn.balibaris.com/23364-home_alternate/mitch-t-shirt-jersey-white.jpg'
